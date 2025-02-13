649cdb3a2a3b5f3cc985a711fc396ce5c6879f39
Cameron Pitsch <cameron.pitsch@rwth-aachen.de>
2025-02-13 18:33:12 +0100

    Allow for spaces in activity names: Need to use string "..." notation

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,9 @@
-// use chrono::Utc;
-// use process_mining::event_log::{Attribute, AttributeValue, Event, Trace, XESEditableAttribute};
-
-// pub fn gen_uuid() -> uuid::Uuid {
-//     uuid::Uuid::new_v4()
-// }
-
-// pub fn create_event(activity: impl Into<String>, offset_s: i64) -> Event {
-//     // let mut evt = Event::new(activity.into());
-//
-//     process_mining::event_log::Event {
-//         attributes: vec![
-//             process_mining::event_log::Attribute::new(
-//                 "time:timestamp".to_string(),
-//                 process_mining::event_log::AttributeValue::Date(
-//                     chrono::Utc::now().fixed_offset() + chrono::TimeDelta::seconds(offset_s),
-//                 ),
-//             ),
-//             process_mining::event_log::Attribute::new(
-//                 "concept:name".to_string(),
-//                 process_mining::event_log::AttributeValue::String(activity.into()),
-//             ),
-//         ],
-//     }
-//
-//     // let mut timestamp = Utc::now().fixed_offset();
-//     // timestamp += chrono::TimeDelta::seconds(offset_s);
-//     //
-//     // evt.attributes.add_attribute(Attribute::new(
-//     //     "time:timestamp".to_string(),
-//     //     AttributeValue::Date(timestamp),
-//     // ));
-//     // evt
-// }
-
-// pub fn create_trace(activities: Vec<impl Into<String>>, case_id: impl Into<String>) -> Trace {
-//     Trace {
-//         attributes: vec![Attribute::new(
-//             "concept:name".to_string(),
-//             AttributeValue::String(case_id.into()),
-//         )],
-//         events: activities
-//             .into_iter()
-//             .enumerate()
-//             .map(|(idx, act)| create_event(act, idx as i64))
-//             .collect(),
-//     }
-// }
-
 #[macro_export]
 macro_rules! event {
     ($name:ident) => {
+        event!(stringify!($name))
+    };
+    ($name:expr) => {
         process_mining::event_log::Event {
             attributes: vec![
                 process_mining::event_log::Attribute::new(
@@ -60,9 +14,7 @@ macro_rules! event {
                 ),
                 process_mining::event_log::Attribute::new(
                     "concept:name".to_string(),
-                    process_mining::event_log::AttributeValue::String(
-                        stringify!($name).to_string(),
-                    ),
+                    process_mining::event_log::AttributeValue::String($name.to_string()),
                 ),
             ],
         }
@@ -71,10 +23,11 @@ macro_rules! event {
 
 #[macro_export]
 macro_rules! trace {
-    ($($name:ident),*) => {
+    ($($name:tt),*) => {
         process_mining::event_log::Trace {
             attributes: vec![process_mining::event_log::Attribute::new(
                 "concept:name".to_string(),
+                // TODO: Use AttributeValue::Id?
                 process_mining::event_log::AttributeValue::String(uuid::Uuid::new_v4().into()),
             )],
             events: [
@@ -83,13 +36,10 @@ macro_rules! trace {
                 evt.attributes
                     .get_by_key_mut("time:timestamp")
                     .unwrap()
+                    // TODO: Could probably just use the existing timestamp and add the timedelta
                     .value = process_mining::event_log::AttributeValue::Date(chrono::Utc::now().fixed_offset() + chrono::TimeDelta::hours(idx as i64));
                 evt
             }).collect()
-            // events: vec![
-            //     // TODO: This does not have an offset...
-            //     $($crate::event!($name)),*
-            // ]
         }
     }
 }
