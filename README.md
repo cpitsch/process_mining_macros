a5cf74a91f3e4a5829ce24767fab1e1c2c172d41
Cameron Pitsch <cameron.pitsch@rwth-aachen.de>
2025-07-29 12:45:42 +0200

    Implement revised macros with custom attributes
    Caution: Implementation assumes the implementation of From<_> for
    AttributeValue, which doesn't exist yet. Nonetheless, when they are
    implemented, all tests pass

--- README.md
+++ README.md
@@ -5,62 +5,59 @@ This crate adds some useful macros for the `process_mining` crate. Specifically,
 
 ## Events
 
-- Events are created based on an activity name and, optionally, a timestamp. By default,
-  the unix epoch is used.
+- Events are created based on an activity name and other optional attributes. 
+  By default, the unix epoch is used as the timestamp.
 
 ```rust
 use process_mining_macros::event;
 
-event!(a); // Creates an event with activity "a"
-event!("more complicated name"); // Creates an event with activity "more complicated name"
-event!(a; timestamp=EPOCH); // Create an event with timestamp 0
-event!(a; timestamp=NOW); // Create an event with the current time as timestamp
+event!("a"); // Creates an event with activity "a"
+event!("a"; {"time:timestamp" => EPOCH}); // Create an event with timestamp 0
+event!("a"; {"time:timestamp" => NOW}); // Create an event with the current time as timestamp
 
 // Use a custom timestamp
 use chrono::{DateTime, FixedOffset};
 let dt: DateTime<FixedOffset> = "2025-01-01T00:00:00+02:00".parse().unwrap();
-event!(a; timestamp=dt); // Create an event with a custom [chrono::Datetime] as timestamp
+event!("a"; {"time:timestamp" => dt}); // Create an event with a custom [chrono::Datetime] as timestamp
 ```
 
 ## Traces
 
 - Traces are a sequence of events, indicated by their activity.
-- The first event has timestamp `base_timestamp`, and each following event is 1 hour
-  later.
-    - `base_timestamp` defaults to unix epoch.
+- Events with no timestamp set are placed 1 hour after the previous events' `time:timestamp`
 
 ```rust
 use process_mining_macros::trace;
 
-trace!(a,b,c,d); // Creates a trace with events with activities "a", "b", "c", and "d"
-trace!(a,b,c,d; base_timestamp=NOW); // Use the current timestamp as the base timestamp of the trace
-trace!(a,b,c,d; base_timestamp=EPOCH); // Use Epoch 0 as the base timestamp of the trace of the trace
+trace!("a","b","c","d"); // Creates a trace with events with activities "a", "b", "c", and "d"
+trace!("a"; {"time:timestamp" => NOW},"b","c","d"); // Use the current timestamp as the base timestamp of the trace
+trace!("a"; {"time:timestamp" => EPOCH},"b","c","d"); // Use Epoch 0 as the base timestamp of the trace of the trace
 
 // Use a custom base timestamp
 use chrono::{DateTime, FixedOffset};
 let dt: DateTime<FixedOffset> = "2025-01-01T00:00:00+02:00".parse().unwrap();
-trace!(a,b,c,d; base_timestamp=dt); // Use a custom [chrono::Datetime] as the base timestamp
+trace!("a"; {"time:timestamp" => dt},"b","c","d"); // Use a custom [chrono::Datetime] as the base timestamp
 ```
 
 ## Event Logs
 
 - Event logs are built by traces, indicated in brackets `[]`.
-- All traces start at `base_timestamp`, which defaults to the unix epoch.
 
 
 ```rust
 use process_mining_macros::event_log;
 
 // Create an event log with two traces
-event_log!([a,b,c,d], [a,c,b,d]);
+event_log!(["a","b","c","d"], ["a","c","b","d"]);
 // Create an event log where all traces start at the current timestamp
-event_log!([a,b,c,d], [a,c,b,d]; base_timestamp=NOW);
-// Create an event log where all traces start at timestamp 0
-event_log!([a,b,c,d], [a,c,b,d]; base_timestamp=EPOCH);
+event_log!(
+  ["a"; {"time:timestamp" => NOW},"b","c","d"],
+  ["a"; {"time:timestamp" => NOW},"c","b","d"],
+);
 
-// Use a custom base timestamp
-use chrono::{DateTime, FixedOffset};
-let dt: DateTime<FixedOffset> = "2025-01-01T00:00:00+02:00".parse().unwrap();
-// Create an event log where all traces start at a custom timestamp
-event_log!([a,b,c,d], [a,c,b,d]; base_timestamp=dt);
+// Create an event log with trace attributes
+event_log!(
+  ["a","b","c","d"] {"concept:name" => "CustomId001"},
+  ["a","c","b","d"] {"concept:name" => "CustomId002", "outcome" => "approved"},
+);
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,73 @@ macro_rules! id_value {
     };
 }
 
+/// A utility macro to expand magic identifiers for attribute values.
+#[macro_export]
+macro_rules! expand_value {
+    (NOW) => {
+        chrono::Utc::now().fixed_offset()
+    };
+    (EPOCH) => {
+        chrono::DateTime::from_timestamp_millis(0)
+            .unwrap()
+            .fixed_offset()
+    };
+    ($val:expr) => {
+        $val
+    };
+}
+
+#[macro_export]
+/// Create an [process_mining::event_log::Event] without adding any automatic attributes
+///
+/// # Examples
+///
+/// ```rust
+/// use process_mining_macros::_event;
+///
+/// _event!("a"); // Creates an event with activity "more complicated name"
+/// // Create an event with the current time as timestamp
+/// _event!("a"; {
+///     "time:timestamp" => NOW
+/// });
+/// // Create an event with timestamp 0
+/// _event!("a"; {
+///     "time:timestamp" => EPOCH
+/// });
+///
+/// use chrono::{DateTime, FixedOffset};
+/// let dt: DateTime<FixedOffset> = "2025-01-01T00:00:00+02:00".parse().unwrap();
+/// // Create an event with a custom [chrono::Datetime] as timestamp
+/// _event!("a"; {
+///     "time:timestamp" => dt
+/// });
+/// ```
+macro_rules! _event {
+    ($name:expr) => {
+        $crate::_event!($name; {})
+    };
+    ($name:expr; { $($key:expr => $val:tt),* $(,)? }) => {
+        process_mining::event_log::Event {
+            attributes: vec![
+                process_mining::event_log::Attribute::new(
+                    "concept:name".to_string(),
+                    process_mining::event_log::AttributeValue::String(
+                        $name.into()
+                    )
+                ),
+                $(
+                    process_mining::event_log::Attribute::new(
+                        $key.into(),
+                        process_mining::event_log::AttributeValue::from(
+                            $crate::expand_value!($val)
+                        )
+                    )
+                ),*
+            ],
+        }
+    };
+}
+
 #[macro_export]
 /// Create an [process_mining::event_log::Event]
 ///
@@ -24,170 +91,188 @@ macro_rules! id_value {
 /// ```rust
 /// use process_mining_macros::event;
 ///
-/// event!(a); // Creates an event with activity "a"
-/// event!("more complicated name"); // Creates an event with activity "more complicated name"
-/// event!(a; timestamp=NOW); // Create an event with the current time as timestamp
-/// event!(a; timestamp=EPOCH); // Create an event with timestamp 0
+/// event!("a"); // Creates an event with activity "more complicated name"
+/// // Create an event with the current time as timestamp
+/// event!("a"; {
+///     "time:timestamp" => NOW
+/// });
+/// // Create an event with timestamp 0
+/// event!("a"; {
+///     "time:timestamp" => EPOCH
+/// });
 ///
 /// use chrono::{DateTime, FixedOffset};
 /// let dt: DateTime<FixedOffset> = "2025-01-01T00:00:00+02:00".parse().unwrap();
-/// event!(a; timestamp=dt); // Create an event with a custom [chrono::Datetime] as timestamp
+/// // Create an event with a custom [chrono::Datetime] as timestamp
+/// event!("a"; {
+///     "time:timestamp" => dt
+/// });
 /// ```
 macro_rules! event {
-    // Convert identifiers to strings
-    ($name:ident) => {
-        $crate::event!(stringify!($name))
-    };
-    ($name:ident; $($rest:tt)*) => {
-        $crate::event!(stringify!($name); $($rest)*)
-    };
-    // Default to timestamp 0 for easier comparisons for tests
-    ($name:expr) => {
-        $crate::event!($name; timestamp=EPOCH)
-    };
-    // Use BASE_EPOCH to use Epoch 0 as the base timestamp
-    ($name:expr; timestamp=EPOCH) => {
-        $crate::event!($name; timestamp=chrono::DateTime::from_timestamp_millis(0).unwrap().fixed_offset())
-    };
-    // Use BASE_NOW to use the current timestamp as the base timestamp
-    ($name:expr; timestamp=NOW) => {
-        $crate::event!($name; timestamp=chrono::Utc::now().fixed_offset())
-    };
-    ($name:expr; timestamp=$timestamp:expr) => {
-        process_mining::event_log::Event {
-            attributes: vec![
+    ($($input:tt)*) => {{
+        let mut evt = $crate::_event!($($input)*);
+
+        if process_mining::event_log::XESEditableAttribute::get_by_key(
+            &evt.attributes,
+            "time:timestamp",
+        )
+        .is_none()
+        {
+            process_mining::event_log::XESEditableAttribute::add_attribute(
+                &mut evt.attributes,
                 process_mining::event_log::Attribute::new(
                     "time:timestamp".to_string(),
-                    process_mining::event_log::AttributeValue::Date($timestamp),
+                    process_mining::event_log::AttributeValue::Date($crate::expand_value!(EPOCH)),
                 ),
+            )
+        }
+        evt
+    }};
+}
+
+#[macro_export]
+macro_rules! trace {
+    (
+        { $($key:expr => $val:expr),* $(,)? };
+        $(
+            $activity:expr $(; { $($keys:expr => $values:tt),* $(,)?})?
+        ),*
+    ) => {{
+        let mut trace = process_mining::event_log::Trace {
+            attributes: vec![
+                $(
+                    process_mining::event_log::Attribute::new(
+                        $key.into(),
+                        process_mining::event_log::AttributeValue::from(
+                            $crate::expand_value!($val)
+                        )
+                    )
+                ),*
+            ],
+            events: vec![
+                $(
+                    $crate::_event!($activity; {
+                        $(
+                            $(
+                                $keys => $values
+                            ),*
+                        )?
+                    })
+                ),*
+            ]
+        };
+
+        let delta = chrono::TimeDelta::hours(1);
+
+        // Make sure the first event has a timestamp, then fill with previous timestamp + 1h
+        if let Some(evt) = trace.events.first_mut() {
+            if process_mining::event_log::XESEditableAttribute::get_by_key(
+                &evt.attributes,
+                "time:timestamp"
+                ).is_none()
+            {
+                process_mining::event_log::XESEditableAttribute::add_attribute(&mut evt.attributes,
+                    process_mining::event_log::Attribute::new(
+                        "time:timestamp".to_string(),
+                        process_mining::event_log::AttributeValue::from(
+                            $crate::expand_value!(EPOCH)
+                        )
+                    )
+                )
+            }
+        }
+
+        for i in 1..trace.events.len() {
+
+            if process_mining::event_log::XESEditableAttribute::get_by_key(&trace.events[i].attributes, "time:timestamp").is_none() {
+                let prev_timestamp = *process_mining::event_log::XESEditableAttribute::get_by_key(&trace.events[i-1].attributes, "time:timestamp").unwrap().value.try_as_date().expect("Timestamp should be a date.");
+                process_mining::event_log::XESEditableAttribute::add_attribute(
+                    &mut trace.events[i].attributes,
+                    process_mining::event_log::Attribute::new(
+                        "time:timestamp".to_string(),
+                        process_mining::event_log::AttributeValue::from(
+                            prev_timestamp + delta,
+                        )
+                    )
+                )
+            }
+        }
+
+
+        if process_mining::event_log::XESEditableAttribute::get_by_key(&trace.attributes, "concept:name").is_none() {
+            process_mining::event_log::XESEditableAttribute::add_attribute(
+                &mut trace.attributes,
                 process_mining::event_log::Attribute::new(
                     "concept:name".to_string(),
-                    process_mining::event_log::AttributeValue::String($name.to_string()),
-                ),
-            ],
+                    $crate::id_value!()
+                )
+            )
         }
-    };
+
+        trace
+    }};
+    ($($content:tt)*) => {
+        $crate::trace!({}; $($content)*)
+    }
 }
 
 #[macro_export]
-/// Create a [process_mining::event_log::Trace]
-///
 /// # Examples
-///
-/// ```rust
-/// use process_mining_macros::trace;
-///
-/// trace!(a,b,c,d); // Creates a trace with events with activities "a", "b", "c", and "d"
-/// trace!(a,b,c,d; base_timestamp=NOW); // Use the current timestamp as the base timestamp of the trace
-/// trace!(a,b,c,d; base_timestamp=EPOCH); // Use Epoch 0 as the base timestamp of the trace of the trace
-///
-/// use chrono::{DateTime, FixedOffset};
-/// let dt: DateTime<FixedOffset> = "2025-01-01T00:00:00+02:00".parse().unwrap();
-/// trace!(a,b,c,d; base_timestamp=dt); // Use a custom [chrono::Datetime] as the base timestamp
-/// ```
-macro_rules! trace {
-    // Temporary (?) fix to make empty trace `trace!()` possible. Note that this does not
-    // allow for constructs such as `trace!(; base_timestamp=NOW)`.
-    () => {
-        process_mining::event_log::Trace {
-            attributes: vec![process_mining::event_log::Attribute::new(
-                "concept:name".to_string(),
-                $crate::id_value!()
-            )],
-            events: vec![]
-        }
-
-    };
-    // Default to EPOCH 0 as the base timestamp for easier comparability in tests
-    // (which is the main use-case of these macros)
-    ($($name:tt),*) => {
-        $crate::trace!($($name),*; base_timestamp=EPOCH)
-    };
-    // Use BASE_EPOCH to use Epoch 0 as the base timestamp
-    ($($name:tt),*; base_timestamp=EPOCH) => {
-        $crate::trace!($($name),*; base_timestamp=chrono::DateTime::from_timestamp_millis(0).unwrap().fixed_offset())
-    };
-    // Use BASE_NOW to use the current timestamp as the base timestamp
-    ($($name:tt),*; base_timestamp=NOW) => {
-        $crate::trace!($($name),*; base_timestamp=chrono::Utc::now().fixed_offset())
-    };
-    ($($name:tt),*; base_timestamp=$base:expr) => {
-        process_mining::event_log::Trace {
-            attributes: vec![process_mining::event_log::Attribute::new(
-                "concept:name".to_string(),
-                $crate::id_value!()
-            )],
-            // TODO: Ideally, instead of setting the timestamp retroactively, it would be passed
-            // straight into the macro, but we have no way of knowing the total number of generated
-            // events
-            // Kind of like: [$($crate::event!($name; timestamp=$base + chrono::TimeDelta::hours($index))),*]
-            // For now, initially use EPOCH since it is likely faster than getting the current time
-            events: [
-                $($crate::event!($name; timestamp=EPOCH)),*
-            ].into_iter().enumerate().map(|(idx, mut evt)| {
-                process_mining::event_log::XESEditableAttribute::get_by_key_mut(&mut evt.attributes, "time:timestamp")
-                    .unwrap()
-                    .value = process_mining::event_log::AttributeValue::Date($base + chrono::TimeDelta::hours(idx as i64));
-                evt
-            }).collect()
-        }
-    };
-}
-
-#[macro_export]
-/// Create an [process_mining::event_log::EventLog].
-///
-/// # Examples
-///
 /// ```rust
 /// use process_mining_macros::event_log;
 ///
-/// // Create an event log with two traces
-/// event_log!([a,b,c,d], [a,c,b,d]);
-/// // Create an event log where all traces start at the current timestamp
-/// event_log!([a,b,c,d], [a,c,b,d]; base_timestamp=NOW);
-/// // Create an event log where all traces start at timestamp 0
-/// event_log!([a,b,c,d], [a,c,b,d]; base_timestamp=EPOCH);
+/// event_log!({};
+///     ["a", "b", "c", "d"],
+///     ["a", "c", "b", "d"],
+/// );
 ///
-/// use chrono::{DateTime, FixedOffset};
-/// let dt: DateTime<FixedOffset> = "2025-01-01T00:00:00+02:00".parse().unwrap();
-/// // Create an event log where all traces start at a custom timestamp
-/// event_log!([a,b,c,d], [a,c,b,d]; base_timestamp=dt);
+///
+/// event_log!(
+///     ["a", "b", "c", "d"],
+///     ["a", "c", "b", "d"],
+/// );
+///
+/// event_log!(
+///     ["a", "b"] { "key1" => "value1", "key2" => "value2" },
+///     ["c"] { "key3" => "value3" },
+///     ["d"],
+/// );
+///
+/// //     ["a","b","c","d"] {"org:resource" => "Cameron"},
+/// event_log!({};
+///    ["a", "b", "c", "d"]
+/// );
+///
 /// ```
-// TODO: something like event_log!([a,b,c,d]*3, [a,c,b,d]*4)
 macro_rules! event_log {
-    // *$(,)? --> Allow trailing comma
-    ($([$($items:tt),*]),*; base_timestamp=EPOCH$(,)?) => {
-        $crate::event_log!($([$($items),*]),*; base_timestamp=chrono::DateTime::from_timestamp_millis(0).unwrap().fixed_offset())
-    };
-    ($([$($items:tt),*]),*; base_timestamp=NOW$(,)?) => {
-        $crate::event_log!($([$($items),*]),*; base_timestamp=chrono::Utc::now().fixed_offset())
-    };
-    ($([$($items:tt),*]),*; base_timestamp=$base:expr) => {
-        process_mining::event_log::EventLog {
-            attributes: process_mining::event_log::Attributes::new(),
+    (
+        $({ $($key:expr => $val:expr),* $(,)? };)?
+        $(
+            [$($events:tt)*] $({ $($keys:expr => $vals:expr),* $(,)? })?
+        ),* $(,)?
+     ) => {
+         process_mining::event_log::EventLog {
+            attributes: vec![
+                $(
+                    $(
+                        process_mining::event_log::Attribute::new(
+                            $key.into(),
+                            process_mining::event_log::AttributeValue::from(
+                                $crate::expand_value!($val)
+                            )
+                        )
+                    ),*
+                )?
+            ],
             traces: vec![
                 $(
-                    $crate::trace!($($items),*; base_timestamp=$base)
+                    $crate::trace!(
+                        $({ $($keys => $vals),*};)?
+                        $($events)*
+                    )
+
                 ),*
-            ]
-            ,
-            extensions: None,
-            classifiers: None,
-            global_trace_attrs: None,
-            global_event_attrs: None,
-        }
-    };
-    // If nothing is specified, just use whatever the default is for traces
-    ($([$($items:tt),*]),*$(,)?) => {
-        process_mining::event_log::EventLog {
-            attributes: process_mining::event_log::Attributes::new(),
-            traces: vec![
-                $(
-                    $crate::trace!($($items),*)
-                ),*
-            ]
-            ,
+            ],
             extensions: None,
             classifiers: None,
             global_trace_attrs: None,
@@ -239,37 +324,22 @@ mod tests {
 
     #[test]
     fn simple_event() {
-        let event_1 = event!(a);
+        let event_1 = event!("a");
         let event_2 = event!("name with spaces");
+        let third_activity = String::from("hello, world!");
+        let event_3 = event!(third_activity);
 
         assert_eq!(event_to_activity(&event_1), "a");
         assert_eq!(event_to_activity(&event_2), "name with spaces");
+        assert_eq!(event_to_activity(&event_3), "hello, world!");
     }
 
     #[test]
     fn timed_event() {
-        let event_1 = event!(a; timestamp=NOW);
-        let event_2 = event!(a; timestamp=EPOCH);
+        let event_1 = event!("a"; {"time:timestamp" => NOW});
+        let event_2 = event!("a"; {"time:timestamp"=> EPOCH});
         let timestamp = chrono::Utc::now().fixed_offset();
-        let event_3 = event!(a; timestamp=timestamp);
-
-        // Some time _will_ have passed since the "now" timestamp was computed, so allow
-        // at most 1s difference (very pessimistic)
-        assert!(
-            chrono::Utc::now().fixed_offset() - event_to_timestamp(&event_1)
-                < chrono::TimeDelta::seconds(1)
-        );
-        assert_eq!(
-            event_to_timestamp(&event_2),
-            chrono::DateTime::from_timestamp_nanos(0)
-        );
-        assert_eq!(event_to_timestamp(&event_3), timestamp);
-
-        // Check the same thing with "name with spaces"
-        let event_1 = event!("name with spaces"; timestamp=NOW);
-        let event_2 = event!("name with spaces"; timestamp=EPOCH);
-        let timestamp = chrono::Utc::now().fixed_offset();
-        let event_3 = event!("name with spaces"; timestamp=timestamp);
+        let event_3 = event!("a"; {"time:timestamp"=> timestamp});
 
         // Some time _will_ have passed since the "now" timestamp was computed, so allow
         // at most 1s difference (very pessimistic)
@@ -286,7 +356,7 @@ mod tests {
 
     #[test]
     fn simple_trace() {
-        let trace = trace!(a, b, c, d);
+        let trace = trace!("a", "b", "c", "d");
         let expected = vec!["a", "b", "c", "d"];
 
         assert_eq!(trace_to_activities(&trace), expected);
@@ -296,12 +366,11 @@ mod tests {
     fn empty_trace() {
         let empty_trace = trace!();
         assert!(empty_trace.events.is_empty());
-        // let empty_trace = trace!(; base_timestamp=chrono::Utc::now().fixed_offset());
     }
 
     #[test]
     fn timed_trace() {
-        let trace_1 = trace!(a,b,c,d; base_timestamp=EPOCH);
+        let trace_1 = trace!("a"; {"time:timestamp" => EPOCH}, "b", "c", "d");
         let epoch = chrono::DateTime::from_timestamp_nanos(0);
         assert_eq!(
             trace_to_timestamps(&trace_1),
@@ -314,7 +383,7 @@ mod tests {
         );
 
         let timestamp = chrono::Utc::now().fixed_offset();
-        let trace_2 = trace!(a,b,c,d; base_timestamp=timestamp);
+        let trace_2 = trace!("a"; {"time:timestamp" => timestamp},"b","c","d");
         assert_eq!(
             trace_to_timestamps(&trace_2),
             vec![
@@ -328,7 +397,11 @@ mod tests {
 
     #[test]
     fn simple_log() {
-        let log = event_log!([a, b, c, d], [a, c, b, d], ["names that", "have spaces"]);
+        let log = event_log!(
+            ["a", "b", "c", "d"],
+            ["a", "c", "b", "d"],
+            ["names that", "have spaces"]
+        );
 
         assert_eq!(
             log_to_activities(&log),
@@ -343,41 +416,5 @@ mod tests {
     #[test]
     fn empty_log() {
         assert!(event_log!().traces.is_empty());
-        assert!(event_log!(; base_timestamp=NOW).traces.is_empty());
-        assert!(event_log!(; base_timestamp=EPOCH).traces.is_empty());
-        assert!(
-            event_log!(; base_timestamp=chrono::Utc::now().fixed_offset())
-                .traces
-                .is_empty()
-        );
-    }
-
-    #[test]
-    /// Test that passing timestamps into event log macros works as expected.
-    ///
-    /// All traces should have this timestamp as their base (starting) timestamp
-    fn timed_log() {
-        let log_now = event_log!([a, b, c, d], [a, c, b, d], ["names that", "have spaces"]; base_timestamp=NOW);
-        let now = chrono::Utc::now().fixed_offset();
-
-        log_now.traces.into_iter().for_each(|trace| {
-            assert!(
-                now - event_to_timestamp(trace.events.first().unwrap())
-                    < chrono::TimeDelta::seconds(1)
-            )
-        });
-
-        let log_epoch = event_log!([a, b, c, d], [a, c, b, d], ["names that", "have spaces"]; base_timestamp=EPOCH);
-        log_epoch.traces.into_iter().all(|trace| {
-            event_to_timestamp(trace.events.first().unwrap())
-                == chrono::DateTime::from_timestamp_nanos(0).fixed_offset()
-        });
-
-        let timestamp = chrono::DateTime::from_timestamp_nanos(1000000000).fixed_offset();
-        let log_custom = event_log!([a, b, c, d], [a, c, b, d], ["names that", "have spaces"]; base_timestamp=timestamp);
-        log_custom
-            .traces
-            .into_iter()
-            .all(|trace| event_to_timestamp(trace.events.first().unwrap()) == timestamp);
     }
 }
